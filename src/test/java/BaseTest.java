151b9f29876b17e2e63dfa49fcaa1fcdb363b8a5
Ivaan1990 <ivaan7845@gmail.com>
2019-03-19 00:58:31 +0300

    Initial commit

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,34 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.concurrent.TimeUnit;
+
+/**
+ *
+ */
+public class BaseTest {
+
+    public WebDriver drv;
+
+    @Before
+    public void initTest(){
+        System.out.println("Travel");
+        System.setProperty("webdriver.chrome.driver", "drv/chromedriver.exe");
+        drv = new ChromeDriver();
+        drv.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        drv.manage().window().maximize();
+        drv.get("https://www.rgs.ru/");
+    }
+
+    @After
+    public void close(){
+        drv.quit();
+    }
+}
---
+++ src/test/java/TravelTest.java
@@ -0,0 +1,111 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.support.ui.Select;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.concurrent.TimeUnit;
+
+/**
+ *
+ */
+public class TravelTest extends BaseTest {
+
+    @Test
+    public void travel(){
+        findElementAndClick("//*/ol/li/a[contains(text(), 'Страхование')]");
+        findElementAndClick("//*[contains(text(), 'Выезжающим за рубеж')]");
+
+        scrollPage("//*[contains(@class, 'thumbnail-footer')]/a[contains(text(),'Рассчитать')]"); /* Скроллер */
+        findElementAndClick("//*[text()=' ок ']");
+        findElementAndClick("//*[contains(@class, 'thumbnail-footer')]/a[contains(text(),'Рассчитать')]");
+
+        scrollPage("//div[@class='page-header']/span [@class ='h1']");
+        /* Поиск нужного текста */
+        Assert.assertEquals(
+                "Страхование выезжающих за рубеж",
+                drv.findElement(By.xpath("//div[@class='page-header']/span [@class ='h1']")).getText().trim()
+        );
+        findElementAndClick("//*[contains(@class, 'btn-content-subtitle small')]");
+
+        scrollPage("//*[contains(@class, 'btn-content-subtitle small')]");
+        fillForm(By.xpath("//*[contains(@class, 'actual-input')]"), "шенген");  // вводим страну
+        findElementAndClick("//*[contains(@class, 'tt-menu tt')]");
+        findElementAndClick("//*[contains(@ id, 'ArrivalCountryList')]");
+        new Select(drv.findElement(By.name("ArrivalCountryList"))).selectByVisibleText("Испания");
+        findElementAndClick("//*[contains(@data-bind, 'value: FirstDepartureDate.')]");
+
+        Itravel dateOfJourney = () -> {
+            LocalDate today = LocalDate.now();
+            LocalDate date = today.plus(14, ChronoUnit.DAYS);
+            String[] split = date.toString().split("-");
+            StringBuilder builder = new StringBuilder();
+            for (int i = split.length - 1; i >= 0; i--) {
+                builder.append(split[i]);
+            }
+            return builder.toString();
+        };
+
+        fillForm(By.xpath("//*[contains(@data-bind, 'value: FirstDepartureDate.')]"), dateOfJourney.function()); // ДАТА ПОЕЗДКИ
+        findElementAndClick("//*[contains(@data-bind, 'btnRadioGroupValue: 90')]");
+
+        scrollPage("//*[contains(@class, 'form-control')][contains(@data-bind, 'attr: _params.fullName.attr,')]");
+        String temp = "//*[contains(@class, 'form-control')][contains(@data-bind, 'attr: _params.fullName.attr,')]";
+        findElementAndClick(temp);
+        fillForm(By.xpath(temp), "Yushin Ivan"); // ФАМИЛИЯ ИМЯ
+        temp = "//*[contains(@data-bind, 'value: BirthDay.computedView')]";
+        findElementAndClick(temp);
+        String birthDate = "01111990";
+        fillForm(By.xpath(temp), birthDate); // ДАТА РОЖДЕНИЯ
+
+        findElementAndClick("//*[contains(text(), 'активный отдых или спорт')]/ancestor::div[@class=\"calc-vzr-toggle-risk-group\"]//div[@class=\"toggle off toggle-rgs\"]");
+        scrollPage("//*[contains(@data-test-name, 'IsProcessingPersonalDataToCalculate')]");
+        findElementAndClick("//*[contains(@data-test-name, 'IsProcessingPersonalDataToCalculate')]");
+        findElementAndClick("//*[contains(@data-bind, 'disable: Misc.NextButtonDisabled')]");
+    }
+
+
+    /**
+     *
+     * Скроллер страницы
+     */
+    public void scrollPage(String xPath){
+        ((JavascriptExecutor)drv).executeScript("arguments[0].scrollIntoView();", drv.findElement(By.xpath(xPath)));
+    }
+
+    /**
+     *
+     * @param locator
+     * @param fill текст который заполняем в форму
+     */
+    public void fillForm(By locator, String fill){
+        drv.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        drv.findElement(locator).click();
+        drv.findElement(locator).clear();
+        drv.findElement(locator).sendKeys(fill);
+    }
+
+    /**
+     *
+     * @param xPath параметр принимает xpath
+     */
+    private void findElementAndClick(String xPath){
+        drv.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        drv.findElement(By.xpath(xPath)).click();
+    }
+
+    /**
+     *
+     * @param text первый параметр текст который хотим найти
+     * @param line второй который извлечён из страницы drv.findElement
+     */
+    private void findText1(String text, String line) {
+        if (text.equalsIgnoreCase(line)){
+            System.out.println("Заголовок соответствует требуемому");
+        } else {
+            System.out.println("Заголовок не соответствует заданному");
+        }
+    }
+}
